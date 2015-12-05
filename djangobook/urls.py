67bb140bf9a408d34d4d32eb9cbd41aa3f5bb2e6
billumandal <billu.mandal@gmail.com>
2015-12-05 14:39:59 +0530

    some more

--- djangobook/urls.py
+++ djangobook/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', 'djangobook.views.hello', name='hello'),
+    url(r'^time/$', 'djangobook.views.current_datetime', name='datetime'),
+    url(r'^time/plus/(\d{1,2})/$', 'djangobook.views.hours_ahead', name='hours_ahead'),
+	url(r'^time/minus/(\d{1,2})/$', 'djangobook.views.hours_behind', name='hours_behind'),
 )
--- djangobook/views.py
+++ djangobook/views.py
@@ -1,7 +1,46 @@
 from django.http import HttpResponse
+from django.template import Template, Context
+from django.template.loader import get_template
+import datetime
 
 def home(request):
 	return HttpResponse("You're in the home of my django book app.")
 
 def hello(request):
 	return HttpResponse("Hello World!")
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	t = Template('current_datetime.html')
+	html = t.render(Context({'current_date': now}))
+	return HttpResponse(html)
+
+def hours_ahead(request, ahead):
+	try:
+		ahead = int(ahead)
+	except ValueError:
+		raise Http404()
+
+	dt = datetime.datetime.now() + datetime.timedelta(hours=ahead)
+	html = """<html>
+				<head><title>Future date & time</title></head>
+				<body>
+					In {} hours, it'll be <strong>{}/{}/{}</strong>, time <i>{}</i> hours <i>{}</i> minutes..
+				</body>
+			</html>""".format(ahead, dt.day, dt.month, dt.year, dt.hour, dt.minute)
+	return HttpResponse(html)
+
+def hours_behind(request, behind):
+	try:
+		behind = int(behind)
+	except ValueError:
+		raise Http404()
+
+	dt = datetime.datetime.now() - datetime.timedelta(hours=behind)
+	html = """<html>
+				<head><title>Earlier date & time</title></head>
+				<body>
+					{} hours ago, it was {}/{}/{}, time {} hours {} minutes..
+				</body>
+			</html>""".format(behind, dt.day, dt.month, dt.year, dt.hour, dt.minute)
+	return HttpResponse(html)
